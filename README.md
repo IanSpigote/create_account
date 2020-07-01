aed9fea314a081ca306b89872ffd77fbe09fbc18
IanSpigote <ian.mapv@gmail.com>
2020-07-01 12:05:50 -0300

    Css e up

--- README.md
+++ README.md
@@ -1,19 +1,36 @@
 # Create Account
 
-![](imagens/inicio.png)
-
+![Tela de Cadastro](imagens/inicio.png)
+![Email já cadastrado](imagens/EmailCadastrado.png)
+![Usuário já existe](imagens/UsuarioExiste.png)
+![As senhas são diferentes](imagens/SenhaDiferente.png)
 ![](imagens/UsuarioCriado.png)
 
-![](imagens/UsuarioExiste.png)
-
-___
-
-## Sobre o projeto
-
-Projeto pessoal simples, para criação de usuários, que verifica se o usuário requerido já existe e emite um alerta que **"Usúario já exite!"** ou  **"Usuário criado com sucesso!"**
+# Sobre o projeto
+Projeto pessoal simples, para criação de usuários, que retorna alertas em caso de falha ou sucesso!
 
 ## Por que?
 O propósito é estudar e aplicar **Java Script** neste projeto, trazendo cada vez mais funcionalidades e tecnologias para ele.
 
+## Observações
+A aplicação não está conectada a um servidor e os dados não são salvos no armazenamento local, portanto os usuários cadastrados resetam quando a página é recarregada.
 
+## Funcionalidades
 
+- O usuário cria uma conta para acessar uma aplicação.
+
+# Iniciando
+
+## Pré-requisitos
+A aplicação não tem nada além de HTML, CSS e Java Script.
+
+## Clonando
+```
+$ git clone https://github.com/IanSpigote/create_account
+```
+
+## Contribuição
+
+Fiquem avontade para enviar PR's, inclusive gostaria muito que fizessem isso. Qualquer duvida,
+
+Me mande um email: ian.start@gmail.com
--- src/main.js
+++ src/main.js
@@ -1,28 +1,59 @@
-var button = document.querySelector('button');
-var inputUser = document.getElementById('input-user');
-var inputSenha = document.getElementById('input-senha');
+class CreateAccount{
+    constructor(){
+        this.form = document.getElementById('register');
+        this.inputUser = document.getElementById('input-user');
+        this.inputSenha = document.getElementById('input-senha');
+        this.inputSame = document.getElementById('confirm-senha')
+        this.inputEmail = document.getElementById('input-email')
+        this.usuarios = [];
 
-var usuarios = [];
-
-button.onclick = () => {
-    let user = inputUser.value;
-    let pass = inputSenha.value;
-    let free = true;
-    let newUser = {
-        user,
-        pass,
+        this.form.onsubmit = (event) =>this.request(event);
     };
-    inputUser.value = '';
-    inputSenha.value = '';
-    for(usuario of usuarios){
-        if(usuario.user === newUser.user){
-            free = false;
-            return(alert('Usuário já existe!'));  
-        }
-    }
+    
+    request(event){
+        event.preventDefault();
+        this.allRight = true;
+        let user = this.inputUser.value;
+        let senha = this.inputSenha.value;
+        let email = this.inputEmail.value;
+        this.newUser = {
+            email,
+            user,
+            senha,
+        };
+        this.validate();
+    };
 
-    if(free === true){
-        usuarios.push(newUser);;
-        alert("Usuário criado com sucesso!");
-    }
+    validate(){
+        if(this.usuarios.length !== 0){
+            for(this.usuario of this.usuarios){  
+                if(this.usuario.email == this.newUser.email){
+                    this.allRight = false;
+                    return alert("Esse email já foi cadastrado!")
+                }
+                if(this.usuario.user === this.newUser.user){
+                    this.allRight = false;
+                    return alert("Usuário já existe!")
+                };
+            };
+        }
+        if(this.inputSenha.value !== this.inputSame.value){
+            this.allRight = false;
+            return alert("As senhas não são iguais!")
+        }
+        this.inputEmail.value = '';
+        this.inputUser.value = '';
+        this.inputSenha.value = '';
+        this.inputSame.value = '';
+        this.result();
+    };
+    result(){
+        if (this.allRight === true){
+            this.usuarios.push(this.newUser);
+            return alert("Usuário criado com sucesso!");
+        };
+    };
 }
+
+const a = new CreateAccount();
+ 
